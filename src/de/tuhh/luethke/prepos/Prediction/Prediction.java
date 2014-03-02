2030f4284973ce1b8784d1701297f339a9e53de6
Jonas Luethke <j.luethke@mytaxi.com>
2014-03-02 16:23:08 +0100

    Deleted unneeded files. Added extraction script.

---
+++ src/de/tuhh/luethke/prepos/Prediction/Prediction.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2014 Jonas Luethke
+ */
+
+package de.tuhh.luethke.prepos.Prediction;
+
+/**
+ * Container for a prediction result.
+ * 
+ * @author Jonas Luethke
+ * 
+ * 
+ */
+public class Prediction {
+	public final double latitude;
+	public final double longitude;
+	public final double marginalProbability;
+	public final double probability;
+	public final double widerProbability;
+	public final double acurracy;
+
+	/**
+	 * Create a new prediction result object.
+	 * 
+	 * @param latitude
+	 * @param longitude
+	 * @param marginalProbability
+	 * @param probability
+	 * @param widerProbability
+	 * @param acurracy
+	 */
+	public Prediction(double latitude, double longitude, double marginalProbability, double probability,
+			double widerProbability, double acurracy) {
+		super();
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.marginalProbability = marginalProbability;
+		this.probability = probability;
+		this.widerProbability = widerProbability;
+		this.acurracy = acurracy;
+	}
+
+}
